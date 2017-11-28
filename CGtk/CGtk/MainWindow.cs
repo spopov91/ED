834b6394e7664679c6e892fce9b70e93576c8e5f
spopov91 <32189265+spopov91@users.noreply.github.com>
2017-11-28 08:46:35 +0100

    CGtk MonoDevelop

---
+++ CGtk/CGtk/MainWindow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow: Gtk.Window
+{
+	public MainWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+		buttonNombre.Clicked += delegate {
+			labelSaludo.Text = "Hola " + entryNombre.Text;
+		};
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+}
