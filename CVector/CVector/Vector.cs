f1a64f8013a154ea2b386c2c4bea835eca841004
spopov91 <32189265+spopov91@users.noreply.github.com>
2018-03-07 13:52:31 +0100

    Add files via upload

---
+++ CVector/CVector/Vector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CVector
+{
+	public class Vector
+	{
+		public static void Sort(int[] v) {
+			
+		}
+
+		public static int IndexMin(int[] v, int initialIndex) {
+			if (initialIndex >= v.Length)
+				throw new IndexOutOfRangeException ();
+			int indexMin=initialIndex;
+			for(int index= initialIndex+1; index <v.Length;index++)
+				if(v[index]< v[indexMin])
+					indexMin=index;
+			return indexMin;
+		}
+
+
+		public static void Swap(int[] v, int index, int otherIndex){
+	}}
+}
+
---
+++ CVector/CVector/VectorTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CVector
+{
+	[TestFixture]
+	public class VectorTest
+	{
+		[Test]
+		public void Sort () {
+			int[] v = {12, 9, 15, 7, 13};
+			Vector.Sort(v);
+			CollectionAssert.AreEqual (new int[]{ 7, 9, 12, 13, 15 }, v);
+			v = new int[]{7, 9, 12, 13, 15};
+			Vector.Sort(v);
+			CollectionAssert.AreEqual (new int[]{ 7, 9, 12, 13, 15 }, v);
+
+		}
+		[Test]
+		public void IndexMin(){
+			int[] v = { 12, 9, 15, 7, 13 };
+			Assert.AreEqual (4, Vector.IndexMin (v, 4));
+			Assert.AreEqual (2, Vector.IndexMin (v, 2));
+			Assert.AreEqual (1, Vector.IndexMin (v, 0));
+
+
+		}
+		[Test]
+		[ExpectedException(typeof(System.IndexOutOfRangeException))]
+		public void IndexMinBadInitialIndexLeft() {
+			int[] v = { 12, 9, 15, 7, 13 };
+			Assert.AreEqual (0, Vector.IndexMin (v, -1));
+
+		}
+		[Test]
+		[ExpectedException(typeof(System.IndexOutOfRangeException))]
+		public void IndexMinBadInitialIndexRight() {
+			int[] v = { 12, 9, 15, 7, 13 };
+			Assert.AreEqual (0, Vector.IndexMin (v, 5));
+		}
+	}
+}
+
